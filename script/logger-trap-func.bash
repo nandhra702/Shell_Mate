eefbccacebd4c23df67fa4a64553d347deedb506
Nandhra702_SukhE <sukhrajsinghnandhra@gmail.com>
2025-07-11 17:23:05 +0530

    Now special message for spelling

--- script/logger-trap-func.bash
+++ script/logger-trap-func.bash
@@ -1,9 +1,11 @@
 log_command() {
-  local cmd="$BASH_COMMAND"
+  local code=$?
+  local cmd
+  cmd=$(fc -ln -1)  # last user command from history
   local formatted_date
   formatted_date=$(date "+%d %B %Y")
 
-  IGNORE_CMDS=("cd" "clear" "history" "cat ~/logger.txt")
+  IGNORE_CMDS=("cd" "clear" "history" "showlog")
   IGNORE_PREFIXES=("ls" "cat" "history" "clear")
 
   for ignored in "${IGNORE_CMDS[@]}"; do
@@ -11,24 +13,25 @@ log_command() {
   done
 
   for prefix in "${IGNORE_PREFIXES[@]}"; do
-    if [[ "$cmd" == "$prefix"* ]]; then
-      return
-    fi
+    [[ "$cmd" == "$prefix"* ]] && return
   done
 
-local venv=""
+  local venv=""
   if [[ -n "$VIRTUAL_ENV" ]]; then
     venv="(venv: $(basename "$VIRTUAL_ENV"))"
   fi
 
   echo "$formatted_date || $(pwd) || $venv: $cmd" >> ~/logger.txt
 
-
   local line_count
   line_count=$(wc -l < ~/logger.txt)
   if (( line_count > 50 )); then
     tail -n +21 ~/logger.txt > ~/.logger_tmp && mv ~/.logger_tmp ~/logger.txt
   fi
+
+  if [[ "$code" =~ ^[0-9]+$ ]] && [ "$code" -eq 127 ]; then
+    echo "COMMAND NOT FOUND. MAYBE A SPELLING MISTAKE?"
+  fi
 }
 
-trap 'log_command' DEBUG
+export PROMPT_COMMAND=log_command
